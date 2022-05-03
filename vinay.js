bb9f5abb4863403beed4a16a5d375bc71bd82e48
vinayjs <100291822+vinayjs@users.noreply.github.com>
2022-05-03 12:20:15 +0530

    Add files via upload

---
+++ vinay.js
@@ -0,0 +1,83 @@
+/*let ten = 10;
+console.log(ten*ten);
+
+let mood= "light";
+console.log(mood);
+
+
+let cars = "AUDI"
+cars = 1 + "AUDI"
+console.log(cars)
+
+
+var myself = "Vinay";
+const greeting = "Hi";
+console.log(greeting + myself);
+
+
+let x = 40;
+console.log("The value of x is",x);
+
+
+
+console.log(Math.max(4,8));
+
+
+
+//let theNumber = 5;
+console.log("the number you picked is the square root of " + theNumber*theNumber);
+
+//if(1 + 1 == 2);
+console.log(true);
+
+//let number =0;
+while(number<=12){
+console.log(number);
+number = number + 2;
+}
+console.log(number);
+
+/*let result = 1;
+let counter = 0;
+while (counter<10){
+    result = result * 2;
+    counter = counter +1;
+}
+console.log(result);
+
+for(let car = 0;car<=12;car=+2){
+    console.log(car);
+}
+console.log();
+
+for(let bike= 20; ;bike = bike +1){
+    if(bike % 7 ==0){
+        console.log(bike);
+        break;
+    }
+
+}
+
+
+for(let plane = 1;plane<=20;plane+=2){
+    console.log(plane);
+}
+
+console.log();
+
+
+const square = function(x){
+    console.log(x*x) ;
+}
+console.log(square(12));
+*/
+
+
+
+
+
+
+
+
+
+
